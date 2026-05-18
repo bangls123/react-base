3c1d52be87c6984160e8e7b671c072c90c7f1e11
bangls123 <lehoangtuan012@gmail.com>
2026-05-18 15:29:36 +0700

    change font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Roboto', 'system-ui', 'sans-serif'],
       },
     },
   },
